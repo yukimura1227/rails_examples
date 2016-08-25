b135e91fa4440dd937e8ee522750518724bda59f
yukimura1227 <takamura1227@gmail.com>
2016-08-25 16:19:11 +0900

    rails new

---
+++ one_to_many_regist_form/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneToManyRegistForm::Application.config.secret_token = 'b57fb56df49191751919e0ea5db1852b54d91c736c852c9eda62aed01519cb58584730222ac432cba0ef56629f0d181e81e9118bd875d3c33e28de1f68d6b4a6'
---
+++ one_to_many_regist_form/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OneToManyRegistForm::Application.config.session_store :cookie_store, key: '_one_to_many_regist_form_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OneToManyRegistForm::Application.config.session_store :active_record_store
