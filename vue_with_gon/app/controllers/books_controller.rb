edb98d04d3dba552eb9250cd0fdcae40a0cc7aab
yukimura1227 <takamura1227@gmail.com>
2017-03-01 18:54:52 +0900

    using gon sample

--- vue_with_gon/app/controllers/books_controller.rb
+++ vue_with_gon/app/controllers/books_controller.rb
@@ -65,6 +65,7 @@ class BooksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_book
       @book = Book.find(params[:id])
+      gon.book_title = @book.title
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
