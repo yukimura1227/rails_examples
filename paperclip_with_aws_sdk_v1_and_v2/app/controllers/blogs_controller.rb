712d45d1334c2525a3bf164bb9b3b1749c9423cd
yukimura1227 <takamura1227@gmail.com>
2017-03-03 20:24:56 +0900

    add feature upload image with aws sdk(cause gem 'aws-sdk', '~> 1')

--- paperclip_with_aws_sdk_v1_and_v2/app/controllers/blogs_controller.rb
+++ paperclip_with_aws_sdk_v1_and_v2/app/controllers/blogs_controller.rb
@@ -69,6 +69,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :contents)
+      params.require(:blog).permit(:title, :contents, :image)
     end
 end
--- paperclip_with_aws_sdk_v1_and_v2/app/models/blog.rb
+++ paperclip_with_aws_sdk_v1_and_v2/app/models/blog.rb
@@ -1,2 +1,17 @@
 class Blog < ActiveRecord::Base
+  has_attached_file :image,
+                    styles: {
+                      thumb: '100x100',
+                      medium: '200x200',
+                      large: '600x400'
+                    },
+                    storage: :s3,
+                    s3_permissions: :private,
+                    s3_credentials: "#{Rails.root}/config/s3.yml",
+                    path: ':attachment/:id/:style.:extension'
+
+  validates_attachment_content_type :image, content_type:
+    [
+      'image/jpg', 'image/jpeg', 'image/png', 'image/gif'
+    ]
 end
