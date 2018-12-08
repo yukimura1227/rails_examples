16568f9de8d840b87fc7ca03aa26ddcc4073b792
yukimura1227 <takamura1227@gmail.com>
2018-12-08 15:38:02 +0900

    chore: rails g devise user

--- wordpress_rails_sso/rails_app/db/schema.rb
+++ wordpress_rails_sso/rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_021046) do
+ActiveRecord::Schema.define(version: 2018_12_08_063753) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 2018_12_04_021046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["blog_id"], name: "index_comments_on_blog_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
