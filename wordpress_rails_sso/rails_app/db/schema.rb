f4bda47761b6ec502aeb75dbfc10e3e660757062
yukimura1227 <takamura1227@gmail.com>
2018-12-24 17:41:10 +0900

    fix: add reuired column

--- wordpress_rails_sso/rails_app/db/schema.rb
+++ wordpress_rails_sso/rails_app/db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_12_24_083400) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
