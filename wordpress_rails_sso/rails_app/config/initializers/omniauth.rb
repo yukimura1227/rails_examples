06afbd2fa2ad835a6a417b9e76745a0712d1bf84
yukimura1227 <takamura1227@gmail.com>
2018-12-24 23:47:30 +0900

    fix: monkey patch

--- wordpress_rails_sso/rails_app/config/initializers/omniauth.rb
+++ wordpress_rails_sso/rails_app/config/initializers/omniauth.rb
@@ -1,1 +1,18 @@
 OmniAuth.config.logger = Rails.logger
+
+# 以下、raw_infoのアクセス先が誤っているようだったのでモンキーパッチ
+module OmniAuth
+  module Strategies
+    class WordpressHosted < OmniAuth::Strategies::OAuth2
+      def raw_info
+        puts access_token.token
+        @raw_info ||= access_token.get(
+          "/oauth/request_access",
+          :params => { 'Authorization' =>
+                       "Bearer #{access_token.token}"
+                       }
+        ).parsed
+      end
+    end
+  end
+end
